19c7b5491b958f7549ff8463cce823e186c00ffb
Zong Xu <zongx98@gmail.com>
2017-10-02 17:38:25 -0400

    Google Cloud Functions

---
+++ routes/images.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+function encodeImg() {
+
+}
+
+router.post('/upload', (req, res, next) => {
+
+
+});
+
+module.exports = router;
