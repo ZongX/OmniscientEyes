4fdab47f87c0bbd25c37ab53bd0102f33d75e053
Jason Zhao <jason@peekapak.com>
2017-10-02 11:57:14 -0400

    Finished welcome page, requires link to other components

--- src/src/container/App.js
+++ src/src/container/App.js
@@ -1,12 +1,41 @@
 import React, { Component } from 'react';
 import { HomePage } from './HomePage.js';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import TransitionGroup from 'react-addons-transition-group';
 import './App.css';
+import welcomeBackground from '../images/space.jpg';
 
 class App extends Component {
+  constructor( props ) {
+    super( props );
+
+    this.state = {
+      isHome: true,
+    }
+
+    this.beginApplication = this.beginApplication.bind( this );
+  }
+
+  beginApplication() {
+    this.setState( {
+      isHome: false,
+    } );
+  }
+
   render() {
     return (
-      <div className="App">
-        <HomePage />
+      <div className="App" style={ { backgroundImage: `url( ${ welcomeBackground } )` } } >
+        <MuiThemeProvider>
+          <TransitionGroup
+            >
+            { ( this.state.isHome ) ?
+              <HomePage
+                beginApplication = { this.beginApplication }
+              /> :
+              null
+            }
+          </TransitionGroup>
+        </MuiThemeProvider>
       </div>
     );
   }
--- src/src/container/HomePage.js
+++ src/src/container/HomePage.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
 import './HomePage.css';
-import { Button } from 'react-bootstrap';
+import RaisedButton from 'material-ui/RaisedButton';
 import { World } from './world';
 
 
 export class HomePage extends Component {
+
   render() {
     return (
       <div id='worldmap'>
-        <Button bsStyle='primary' bsSize='large' block>Translate Now</Button>
         <World />
+        <RaisedButton
+          className='TranslateButton'
+          label='Translate Now'
+          backgroundColor='#2266BB'
+          labelColor='#FFFFFF'
+          buttonStyle={ { borderRadius: 25 } }
+          style={ { margin: 12, borderRadius: 25 } }
+          labelPosition='before'
+          containerElement='label'
+          onClick={
+            () => this.props.beginApplication()
+          }
+        />
       </div>
     );
   }
