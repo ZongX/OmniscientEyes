b09e1d8f8c7571eb150e52a967041f1ca46d21f0
Zong Xu <zongx98@gmail.com>
2017-10-02 23:34:05 -0400

    Implemented extraction of raw text from image; working on translating text

--- routes/images.js
+++ routes/images.js
@@ -1,13 +1,39 @@
 const express = require('express');
 const router = express.Router();
+var request = require('request');
+var fs = require('fs');
+const Vision = require('@google-cloud/vision');
 
-function encodeImg() {
+const vision = Vision({
+  keyFilename: './keyfile.json'
+});
 
+function extractText(imgFilePath) {
+  return new Promise((resolve, reject) => {
+    var imageSrcObj = {
+      source: { filename: "./imgs/28.jpg" }
+    }
+    vision.textDetection(imageSrcObj).then((results) => {
+      const detections = results[0].textAnnotations;
+      console.log('Text:');
+      detections.forEach((text) => console.log(text));
+      resolve(detections);
+    }).catch((err) => {
+      console.error('ERROR:', err);
+      reject(err);
+    });
+  });
 }
 
 router.post('/upload', (req, res, next) => {
-
-
+  // Need to use Vision API to extract the text from image.
+  var filePath = './imgs/28.jpg';
+  extractText(filePath).then((detections) => {
+    var detectionsObj = {
+      "detections": detections
+    }
+    res.send(detectionsObj);
+  });
 });
 
 module.exports = router;
